1038cb085c05a8b42984b6bc0b3599bcad96bd26
KumJungMin <rmawjdals@naver.com>
2024-09-26 23:08:13 +0900

    test

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const rules = require("./lib/rules");
 
 const plugin = {
-  rules,
-  rulesConfig: {
-    "no-object-hasown": 2,
-    "no-console": 2,
+  rules, // 규칙 정의
+  configs: {
+    recommended: {
+      rules: {
+        "my-eslint-plugin/no-object-hasown": "error",
+        "my-eslint-plugin/no-console": "error",
+      },
+    },
   },
 };
 
