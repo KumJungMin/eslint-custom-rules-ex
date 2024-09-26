6a2232220eaba3f3046a91553167df9d0b7a78e3
KumJungMin <rmawjdals@naver.com>
2024-09-26 21:39:31 +0900

    feat: add no-object-hasown rules

---
+++ tests/no-object-hasown.test.js
@@ -0,0 +1,24 @@
+const { RuleTester } = require("eslint");
+const rule = require("../lib/rules/no-object-hasown");
+
+const ruleTester = new RuleTester();
+
+ruleTester.run("no-object-hasown", rule, {
+  valid: [
+    {
+      code: 'const hasProperty = "prop" in obj',
+    },
+  ],
+  invalid: [
+    {
+      code: 'Object.hasOwn(obj, "prop")',
+      errors: [
+        {
+          message:
+            "호환성 오류: 모바일 최소 버전(Andriod 6.0, IOS 12.0)에서 지원하지 않는 문법입니다.",
+        },
+      ],
+      output: '"prop" in obj',
+    },
+  ],
+});
